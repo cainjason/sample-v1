a1bd574a554f47591ede3e9a9da4fc16cb44009b
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-02 09:58:44 -0500

    Chore: updated react-player

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Body from './Components/Body';
 function App() {
   return (
     <div className="app">
-     <h2>Sample</h2>
      <Body />
     </div>
   );
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,24 @@
-import React from 'react'
-// import ReactPlayer from 'react-player';
+import React from 'react';
+import ReactPlayer from 'react-player';
 import styled from 'styled-components';
+
 
 function Body() {
   return (
     <ContainerBody>
         <h2> body </h2>
-      {/* <ReactPlayer /> */}
+        <ReactPlayer
+        
+            url='https://fb.watch/oZ9mRra7eb/'
+            controls
+            muted
+            upload
+            config={{
+              youtube: {
+                playerVars: { showinfo: 1 }
+              }
+            }}
+          />
     </ContainerBody>
   )
 }
@@ -14,7 +26,12 @@ function Body() {
 export default Body;
 
 const ContainerBody = styled.div`
-    background-color: gray;
     height: 100vh;
     width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: gray;
 `;
+
+
